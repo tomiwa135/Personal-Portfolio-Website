63c83e70e7f74c67efd8a71aca1fee3c761ca287
Tomiwa Akinrotim <tomiwa.akinrotimi@gmail.com>
2023-12-18 16:56:50 -0600

    Added ServiceNow integration when the contact form is submitted, it automatically creates a new record in a ServiceNow 'ContactForm' record using the POST method in the ServiceNow REST API Explorer

--- script.js
+++ script.js
@@ -18,3 +18,29 @@ function promptUser() {
         alert("Wrong Password!");
     }
 }
+
+const form = document.querySelector('.contact-right-form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    var client=new XMLHttpRequest();
+    client.open("post","https://dev75340.service-now.com/api/now/table/u_contactform");
+
+    client.setRequestHeader('Accept','application/json');
+    client.setRequestHeader('Content-Type','application/json');
+
+    client.setRequestHeader('Authorization', 'Basic '+btoa('admin'+':'+'2k^@zIV6ehBC'));
+
+    client.onreadystatechange = function() { 
+        if(client.status = 201 && client.readyState == 4) {
+            console.log("Form submitted successfully");
+        }
+    }; 
+    client.send(JSON.stringify(data));
+
+});
+
