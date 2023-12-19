9ccaa71eeee4b16a3e7ee454823a1fda63547017
Tomiwa Akinrotim <tomiwa.akinrotimi@gmail.com>
2023-12-18 19:37:39 -0600

    Clean-up and added some extra features so the links open up in a new page

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ function downloadResume() {
     window.open('assets/tomiwa-Akinrotimi-Resume.pdf');
 }
 
+function openLink(url) {
+    window.open(url, '_blank');
+}
+
 function promptUser() {
     var operations = prompt("Please enter password:");
 
@@ -28,23 +32,23 @@ form.addEventListener('submit', event => {
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
 
-    var client=new XMLHttpRequest();
-    client.open("post","https://dev75340.service-now.com/api/now/table/u_contactform");
+    var client = new XMLHttpRequest();
+    client.open("post", "https://dev75340.service-now.com/api/now/table/u_contactform");
 
-    client.setRequestHeader('Accept','application/json');
-    client.setRequestHeader('Content-Type','application/json');
+    client.setRequestHeader('Accept', 'application/json');
+    client.setRequestHeader('Content-Type', 'application/json');
 
-    client.setRequestHeader('Authorization', 'Basic '+btoa('admin'+':'+'2k^@zIV6ehBC'));
+    client.setRequestHeader('Authorization', 'Basic ' + btoa('admin' + ':' + '2k^@zIV6ehBC'));
 
-    client.onreadystatechange = function() { 
-        if(client.status = 201 && client.readyState == 4) {
+    client.onreadystatechange = function () {
+        if (client.status = 201 && client.readyState == 4) {
             msg.innerHTML = "Message sent successfully!"
-            setTimeout(()=>{
-                msg.innerHTML="";
+            setTimeout(() => {
+                msg.innerHTML = "";
             }, 3000)
             form.reset()
         }
-    }; 
+    };
     client.send(JSON.stringify(data));
 
 });
