4b0d5c9e6be3f541521d056df57fcddbbb7c7f6a
Tomiwa Akinrotim <tomiwa.akinrotimi@gmail.com>
2024-01-26 04:48:34 -0600

    contact form first session

--- js/script.js
+++ js/script.js
@@ -28,7 +28,8 @@ form.addEventListener('submit', event => {
     client.setRequestHeader('Accept', 'application/json');
     client.setRequestHeader('Content-Type', 'application/json');
 
-    client.setRequestHeader('Authorization', 'Basic ' + btoa('admin' + ':' + '2k^@zIV6ehBC'));
+    var bearer = 'JeGB51ZdVCn-vjJgPijpPvcQe1swZbLFKW97K9J54686Z9woY8DNkltYFvdO-vF7Sxpt-af0Q5aKqzrO-fwryA'
+    client.setRequestHeader('Authorization', 'Bearer ' + bearer);
 
     client.onreadystatechange = function () {
         if (client.status = 201 && client.readyState == 4) {
@@ -42,3 +43,96 @@ form.addEventListener('submit', event => {
     client.send(JSON.stringify(data));
 
 });
+
+var nameError = document.getElementById("name-error");
+var phoneError = document.getElementById("phone-error");
+var emailError = document.getElementById("email-error");
+var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
+
+function validateName() {
+    var name = document.getElementById('contact-name').value;
+
+    if (name.length == 0) {
+        nameError.innerHTML = 'Please enter your name';
+        return false;
+    }
+
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = 'Please enter your full name';
+        return false;
+    }
+
+    nameError.innerHTML = '<i class="ri-checkbox-circle-fill"></i>';
+    return true;
+}
+
+function validatePhone() {
+    var phone = document.getElementById('contact-phone').value;
+
+    if (phone.length == 0) {
+        phoneError.innerHTML = 'Phone No. is required';
+        return false;
+    }
+
+    if (phone.length !== 10) {
+        phoneError.innerHTML = 'Phone No. should be 10 digits';
+        return false;
+    }
+
+    if (!phone.match(/^\d{10}$/)) {
+        phoneError.innerHTML = 'Phone No. should be 10 digits';
+        return false;
+    }
+
+    phoneError.innerHTML = '<i class="ri-checkbox-circle-fill"></i>';
+    return true;
+}
+
+function validateEmail() {
+    var email = document.getElementById('contact-email').value;
+
+    if (email.length == 0) {
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+
+    if (!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
+        emailError.innerHTML = 'Email invalid';
+        return false;
+    }
+
+    emailError.innerHTML = '<i class="ri-checkbox-circle-fill"></i>';
+    return true;
+}
+
+function validateMessage() {
+    var message = document.getElementById('contact-message').value;
+    var required = 30;
+    var left = required - message.length;
+
+    if (left > 0) {
+        messageError.innerHTML = left + ' more characters left';
+        return false;
+    }
+
+    if (message.length == 0) {
+        messageError.innerHTML = 'Please enter your message';
+        return false;
+    }
+
+    messageError.innerHTML = '<i class="ri-checkbox-circle-fill"></i>';
+    return true;
+}
+
+function validateForm() {
+    if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fix error to submit';
+        setTimeout(()=>{
+            submitError.style.display = 'none';
+        },3000);
+        return false;
+    }
+}
+
