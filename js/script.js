e48fb149e3bb0504864fbf1ff7c535190704647d
Tomiwa Akinrotim <tomiwa.akinrotimi@gmail.com>
2024-11-11 20:35:42 -0600

    Changed resume

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function toggleMenu() {
 }
 
 function downloadResume() {
-    window.open('assets/Tomiwa_Akinrotimi_Resume.pdf');
+    window.open('assets/Tomiwa_Akinrotimi_Resume_01.pdf');
 }
 
 function downloadITIL() {
